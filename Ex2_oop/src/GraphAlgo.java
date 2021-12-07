a700d62e249ac7e284deac16e35279741d9f9a83
Eitan Wechsler <wexeitan@gmail.com>
2021-12-07 12:35:52 +0200

    Update GraphAlgo.java

--- Ex2_oop/src/GraphAlgo.java
+++ Ex2_oop/src/GraphAlgo.java
@@ -1,4 +1,7 @@
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class GraphAlgo implements DirectedWeightedGraphAlgorithms {
     private DirectedWeightedGraph graph;
@@ -25,12 +28,40 @@ public class GraphAlgo implements DirectedWeightedGraphAlgorithms {
 
     @Override
     public boolean isConnected() {
-        return false;
+        if (this.graph.nodeSize() == 0) {
+            return true;
+        }
+        for (Iterator<NodeData> it = this.graph.nodeIter(); it.hasNext(); ) {
+            Node_Data n = (Node_Data) it.next();
+            boolean b = this.bfs(n);
+            resetTag();
+            if (!b) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public double shortestPathDist(int src, int dest) {
-        return 0;
+        if (this.graph.edgeIter()| this.graph.getV().contains(dest)) {
+            throw new RuntimeException("One or more of your keys does not exist in the graph!");
+        }
+        if(src == dest){
+            return 0;
+        }
+        resetInfo();
+        resetTag();
+        resetWeight();
+        double d = Dijkstra(this.graph.getNode(src), this.graph.getNode(dest));
+        resetInfo();
+        resetTag();
+        resetWeight();
+        if (d == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return d;
+
     }
 
     @Override
@@ -56,5 +87,32 @@ public class GraphAlgo implements DirectedWeightedGraphAlgorithms {
     @Override
     public boolean load(String file) {
         return false;
+    }
+
+    private void resetTag() {
+        for (Iterator<NodeData> it = this.graph.nodeIter(); it.hasNext(); ) {
+            Node_Data n = (Node_Data) it.next();
+            n.setTag(-1);
+        }
+    }
+    private boolean bfs(Node_Data n) {
+        Queue<Node_Data> queue = new LinkedList<>();
+        n.setTag(1);
+        int counter = 1;
+        queue.add(n);
+        while (!queue.isEmpty()) {
+            Node_Data temp = queue.poll();
+            Iterator<EdgeData> edges = this.graph.edgeIter(temp.getKey());
+            for (Iterator<EdgeData> it = edges; it.hasNext(); ) {
+                edge next = (edge) it.next();
+                Node_Data dest = (Node_Data) this.graph.getNode(next.getDest());
+                if (dest.getTag() == -1) {
+                    dest.setTag(1);
+                    queue.add(dest);
+                    counter++;
+                }
+            }
+        }
+        return (counter == this.graph.nodeSize());
     }
 }
